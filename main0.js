98de40a3e3a9547367d2b48ef3f13897bb60250f
meol <eumeol@gmail.com>
2018-12-13 21:46:03 +0000

    done code 2 - return email good

---
+++ main0.js
@@ -0,0 +1,23 @@
+const btn=document.querySelector("#getJoke");
+const output = document.querySelector('#output');
+console.log(btn);
+btn.addEventListener("click", getJoke);
+
+
+function getJoke(){
+  const xhr = new XMLHttpRequest();
+  const url = 'https://api.chucknorris.io/jokes/random'
+
+  xhr.onreadystatechange = ()=>{
+    if (xhr.readyState===4 && xhr.status===200){
+      const strR = xhr.responseText;
+      const objR = JSON.parse(strR);
+      output.innerHTML = objR.value;
+    }
+  }
+
+  xhr.open('GET',url);
+  xhr.send();
+  console.log(xhr);
+
+}
--- public/main.js
+++ public/main.js
@@ -1,23 +1,28 @@
-const btn=document.querySelector("#getJoke");
-const output = document.querySelector('#output');
-console.log(btn);
-btn.addEventListener("click", getJoke);
+const btn = document.querySelector("button");
+const output =document.querySelector("#output");
+const intake = document.querySelector("input");
+let url = "https://randomuser.me/api";
 
+btn.addEventListener("click", getInput);
 
-function getJoke(){
+function getInput() {
   const xhr = new XMLHttpRequest();
-  const url = 'https://api.chucknorris.io/jokes/random'
-
-  xhr.onreadystatechange = ()=>{
-    if (xhr.readyState==4 && xhr.status==200){
-      const strR = xhr.responseText;
-      const objR = JSON.parse(strR);
-      output.innerHTML = objR.value;
+  let tempIntake = intake.value;
+  let tempUrl = url + "?results=" + tempIntake;
+  xhr.onload = function(){
+    if (xhr.readyState===4 && xhr.status=="200"){
+      let data = JSON.parse(xhr.responseText).results;
+      outputHtml(data);
     }
   }
 
-  xhr.open('GET',url);
+  xhr.open("GET", tempUrl);
   xhr.send();
-  console.log(xhr);
+  // console.log(xhr);
+}
 
+function outputHtml(data) {
+  data.map((d)=>{
+    output.innerHTML += d.email + '<br/>';
+  });
 }
