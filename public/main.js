f510aae1c74487056827de868e1cc2d14d49ccf8
meol <eumeol@gmail.com>
2018-12-13 20:57:13 +0000

    good html set up

--- public/main.js
+++ public/main.js
@@ -1,15 +1,23 @@
-const xhr = new XMLHttpRequest();
-const url = 'https://api.chucknorris.io/jokes/random';
-xhr.onreadystatechange = ()=>{
-  if(xhr.readyState==4 && xhr.status==200){
-    // console.log(xhr.responseText);
-    const responseStr = xhr.responseText;
-    const responseObj = JSON.parse(responseStr);
-    console.log(responseObj);
+const btn=document.querySelector("#getJoke");
+const output = document.querySelector('#output');
+console.log(btn);
+btn.addEventListener("click", getJoke);
+
+
+function getJoke(){
+  const xhr = new XMLHttpRequest();
+  const url = 'https://api.chucknorris.io/jokes/random'
+
+  xhr.onreadystatechange = ()=>{
+    if (xhr.readyState==4 && xhr.status==200){
+      const strR = xhr.responseText;
+      const objR = JSON.parse(strR);
+      output.innerHTML = objR.value;
+    }
   }
 
-}  
+  xhr.open('GET',url);
+  xhr.send();
+  console.log(xhr);
 
-xhr.open('GET', url);
-xhr.send();
-// console.log(xhr);
+}
